b01c4270070e0cb90fa22aa49378f91fff450f2d
atharkn7 <athar.kn7@gmail.com>
2025-11-01 02:24:02 +0530

    Updated popup

--- popup/script.js
+++ popup/script.js
@@ -1,14 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    document.querySelector('button').onclick = () => {
-
-        const heading = document.querySelector('h1');
-
-        if (heading.innerHTML === 'Hello') {
-            heading.innerHTML = 'Bye';
-        } else {
-            heading.innerHTML = 'Hello';
-        }
-
+// Changes heading on some event
+function hello() {
+    const heading = document.querySelector('h1');
+    if (heading.innerHTML === 'Hello') {
+        heading.innerHTML = 'Bye';
+    } else {
+        heading.innerHTML = 'Hello';
     }
+}
+
+// Alert users name from form
+function greet() {
+    const name = document.querySelector('#name').value;
+    alert(`Hello, ${name}`)
+}
+
+
+// Running functions after DOM Content 
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('button').onclick = hello;
+    document.querySelector('form').onsubmit = greet;
 })
