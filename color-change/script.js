1eb206f84c0e8dd6f4c58b0cfcea65b783ed94da
atharkn7 <athar.kn7@gmail.com>
2025-11-02 20:27:59 +0530

    Added earlier code in comments

--- color-change/script.js
+++ color-change/script.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    /* Earlier workflow using buttons
+    
+    document.querySelectorAll('button').forEach((button) => {
+        // Running onclick on each interation
+        button.onclick = () => {
+            // Setting dataset value as the color of each button
+            document.querySelector('#circle').style.backgroundColor = button.dataset.color;
+    */
+
     document.querySelector('select').onchange = function () {
-        // Setting dataset value as the color of each button
+        // Using 'this' we get value from <option>
         document.querySelector('#circle').style.backgroundColor = this.value;
     }
-})
+});
+
