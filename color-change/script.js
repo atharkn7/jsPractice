414e2e5ccbd5598d227b9ccf4fbf205b99de6e48
atharkn7 <athar.kn7@gmail.com>
2025-11-02 20:20:55 +0530

    Updated colors

--- color-change/script.js
+++ color-change/script.js
@@ -1,11 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Getting a NodeList of all buttons and looping
-    // Kinda same as for button in buttons: (in pyton)
-    document.querySelectorAll('button').forEach((button) => {
-        // Running onclick on each interation
-        button.onclick = () => {
-            // Setting dataset value as the color of each button
-            document.querySelector('#circle').style.backgroundColor = button.dataset.color;
-        }
-    })
+    document.querySelector('select').onchange = function () {
+        // Setting dataset value as the color of each button
+        document.querySelector('#circle').style.backgroundColor = this.value;
+    }
 })
